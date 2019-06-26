8f0cbb419a76d27df5ac5c7742cf1906763f50fa
orthagonal <orthagonal1@gmail.com>
2019-06-26 04:32:36 +0000

    set response headers and cookie

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-module.exports.html = (body, statusCode = 200) => ({
-  headers: { 'content-type': 'text/html; charset=utf8' },
-  body,
-  statusCode
-});
+module.exports.html = (body, statusCode = 200, response = {}, cookie = false) => {
+  response.headers = Object.assign({ 'content-type': 'text/html; charset=utf8' }, response.headers);
+  response.body = body;
+  response.statusCode = statusCode;
+  // arc.codes will handle the 'cookie' field:
+  if (cookie) {
+    response.cookie = cookie;
+  }
+  return response;
+};
 
-module.exports.json = (body, statusCode = 200) => ({
-  headers: { 'content-type': 'application/json; charset=utf8' },
-  body: JSON.stringify(body),
-  statusCode
-});
+module.exports.json = (body, statusCode = 200, response = {}, cookie = false) => {
+  response.headers = Object.assign({ 'content-type': 'application/json; charset=utf8' }, response.headers);
+  response.body = JSON.stringify(body);
+  response.statusCode = statusCode;
+  if (cookie) {
+    response.cookie = cookie;
+  }
+  return response;
+};
 
-module.exports.redirect = (location, type = 'permanent') => ({
-  headers: {
-    Location: location,
-  },
-  statusCode: type === 'permanent' ? 301 : 302
-});
+module.exports.redirect = (location, type = 'permanent', response = {}, cookie = false) => {
+  response.headers = Object.assign({ Location: location }, response.headers);
+  response.statusCode = type === 'permanent' ? 301 : 302;
+  if (cookie) {
+    response.cookie = cookie;
+  }
+  return response;
+};
--- test/test.replies.js
+++ test/test.replies.js
@@ -10,12 +10,41 @@ tap.test('http', t => {
   });
   t.end();
 });
+
+tap.test('http w cookies and header', t => {
+  const r = reply.html('<body> hi </body>', 201, { headers: { 'blah-blah': 'blah' } }, 'token=blah123');
+  t.match(r, {
+    headers: {
+      'content-type': 'text/html; charset=utf8',
+      'blah-blah': 'blah'
+    },
+    body: '<body> hi </body>',
+    statusCode: 201,
+    cookie: 'token=blah123'
+  });
+  t.end();
+});
+
 tap.test('json', t => {
   const r = reply.json({ packet: 'crisps' });
   t.match(r, {
     headers: { 'content-type': 'application/json; charset=utf8' },
     body: JSON.stringify({ packet: 'crisps' }),
     statusCode: 200
+  });
+  t.end();
+});
+
+tap.test('json w cookies and header', t => {
+  const r = reply.json({ packet: 'crisps' }, 200, { headers: { 'blah-blah': 'blah' } }, 'token=blah123');
+  t.match(r, {
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'blah-blah': 'blah'
+    },
+    body: '{"packet":"crisps"}',
+    statusCode: 200,
+    cookie: 'token=blah123'
   });
   t.end();
 });
@@ -34,6 +63,26 @@ tap.test('redirect', t => {
       Location: 'https://google.com',
     },
     statusCode: 302
+  });
+  t.end();
+});
+
+tap.test('redirect w cookies and header', t => {
+  const perm = reply.redirect('https://google.com', 'permanent', { headers: { 'blah-blah': 'blah' } }, 'token=blah123');
+  const temp = reply.redirect('https://google.com', 'temporary', { headers: { 'blah-blah': 'blah' } }, 'token=blah123');
+  t.match(perm, {
+    headers: {
+      Location: 'https://google.com', 'blah-blah': 'blah'
+    },
+    statusCode: 301,
+    cookie: 'token=blah123'
+  });
+  t.match(temp, {
+    headers: {
+      Location: 'https://google.com', 'blah-blah': 'blah'
+    },
+    statusCode: 302,
+    cookie: 'token=blah123'
   });
   t.end();
 });
