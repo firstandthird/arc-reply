92316e2546fa7a0fefc038b4570feffebe963094
orthagonal <orthagonal1@gmail.com>
2019-09-20 01:28:16 +0000

    change params

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
-module.exports.html = (body, statusCode = 200, response = {}, cookie = false) => {
+const objectToCookie = (obj) => {
+  const cookies = [];
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === 'object') {
+      Object.keys(obj[key]).forEach(subkey => {
+        cookies.push(`${subkey}=${obj[key][subkey]}`);
+      });
+    } else {
+      cookies.push(`${key}=${obj[key]}`);
+    }
+  });
+  return cookies.join('; ');
+};
+
+const setResponse = (options = {}) => {
+  const response = {};
+  response.headers = options.headers;
+  response.statusCode = options.statusCode || 200;
+  if (options.cookies) {
+    response.headers['Set-Cookie'] = objectToCookie(options.cookies);
+  }
+  if (options.expires) {
+    response.headers['cache-control'] = `max-age=${options.expires}`;
+  }
+  if (options.cors) {
+    response.headers['access-control-allow-origin'] = '*';
+  }
+  return response;
+};
+
+module.exports.html = (body, options) => {
+  const response = setResponse(options);
   response.headers = Object.assign({ 'content-type': 'text/html; charset=utf8' }, response.headers);
   response.body = body;
-  response.statusCode = statusCode;
-  // arc.codes will handle the 'cookie' field:
-  if (cookie) {
-    response.cookie = cookie;
-  }
   return response;
 };
 
-module.exports.json = (body, statusCode = 200, response = {}, cookie = false) => {
+module.exports.json = (body, options) => {
+  const response = setResponse(options);
   response.headers = Object.assign({ 'content-type': 'application/json; charset=utf8' }, response.headers);
   response.body = JSON.stringify(body);
-  response.statusCode = statusCode;
-  if (cookie) {
-    response.cookie = cookie;
-  }
   return response;
 };
 
-module.exports.text = (body, statusCode = 200, response = {}, cookie = false) => {
+module.exports.text = (body, options) => {
+  const response = setResponse(options);
   response.headers = Object.assign({ 'content-type': 'application/text; charset=utf8' }, response.headers);
   response.body = body;
-  response.statusCode = statusCode;
-  if (cookie) {
-    response.cookie = cookie;
-  }
   return response;
 };
 
-module.exports.redirect = (location, type = 'permanent', response = {}, cookie = false) => {
+module.exports.redirect = (location, options) => {
+  const response = setResponse(options);
   response.headers = Object.assign({ Location: location }, response.headers);
-  response.statusCode = type === 'permanent' ? 301 : 302;
-  if (cookie) {
-    response.cookie = cookie;
-  }
+  response.statusCode = options.type === 'permanent' ? 301 : 302;
   return response;
 };
